f5ca858b446ba52c8abf4160cd71dcb0d1d357b8
Yuma <yuma.kitsu22@gmail.com>
2022-04-20 21:53:05 +0900

    Connect to MySQL

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clustering',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'xLceeRZc'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
