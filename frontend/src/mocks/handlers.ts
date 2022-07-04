e61913df9b21fad31671b69cce8d334f5e514e01
Yuma <yuma.kitsu22@gmail.com>
2022-07-05 06:09:17 +0900

    Add Result page testing

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,3 +1,4 @@
+import createMixins from '@mui/material/styles/createMixins';
 import { rest } from 'msw';
 
 export const handlers = [
@@ -31,5 +32,74 @@ export const handlers = [
                     ]
                 })
         )
-    })
+    }),
+    rest.get('http://127.0.0.1:8000/api/results/:pref/:cluster', (req, res, ctx) => {
+        const prefecture = req.url.searchParams.get('pref')
+        const cluster = req.url.searchParams.get('cluster')
+        return res(
+            ctx.json({prefecture}),
+            ctx.json(
+                {
+                    results: [
+                        {
+                            id: 11,
+                            prefecture: "北海道",
+                            city: "函館市",
+                            ward: null,
+                            total_population: 265979,
+                            those_under_15: 27131,
+                            those_between_15_and_64: 152154,
+                            those_over_65: 85931,
+                            kindergartens: 23,
+                            elementary_schools: 48,
+                            junior_high_schools: 32,
+                            high_schools: 15,
+                            hospitals: 29,
+                            population_trends: -4.71,
+                            move_in_ratio: 3.38,
+                            move_out_ratio: 3.72,
+                            primary_industry_ratio: 3.53,
+                            secondary_industry_ratio: 16.64,
+                            tertiary_industry_ratio: 73.84,
+                            cluster: 3
+                        }
+                    ]
+                })
+        )
+    }),
+    rest.get('http://127.0.0.1:8000/api/results/:city', (req, res, ctx) => {
+        const cluster = req.url.searchParams.get('city')
+        return res(
+            ctx.json(
+                {
+                    results: [
+                        {
+                            id: 11,
+                            prefecture: "北海道",
+                            city: "函館市",
+                            ward: null,
+                            total_population: 265979,
+                            those_under_15: 27131,
+                            those_between_15_and_64: 152154,
+                            those_over_65: 85931,
+                            kindergartens: 23,
+                            elementary_schools: 48,
+                            junior_high_schools: 32,
+                            high_schools: 15,
+                            hospitals: 29,
+                            population_trends: -4.71,
+                            move_in_ratio: 3.38,
+                            move_out_ratio: 3.72,
+                            primary_industry_ratio: 3.53,
+                            secondary_industry_ratio: 16.64,
+                            tertiary_industry_ratio: 73.84,
+                            cluster: 3
+                        },
+
+                    
+                    ]
+                })
+        )
+    }),
+
 ]
